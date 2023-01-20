ce06be91df23f8bffa8f0656df0b1adedac53096
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-01-20 01:02:00 +0100

    state formData

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Text } from '../components/atoms'
 import { Body } from '../components/layout'
@@ -5,11 +6,11 @@ import { InputTextGroup } from '../components/molecules'
 import { selectUser } from '../store/userSlice'
 import { Container, FlexBox } from '../styles'
 
-const handleChange = _ => {}
 
 // eslint-disable-next-line react/function-component-definition
 const UserProfile = () => {
   const user = useSelector(selectUser)
+
   return (
     <FlexBox>
       <Text>
@@ -22,12 +23,32 @@ const UserProfile = () => {
   )
 }
 // eslint-disable-next-line react/function-component-definition
-const UpdateForm = () => (
-  <FlexBox as="form">
-    <InputTextGroup label="Nombre" id="nombre" onChange={handleChange} />
-    <InputTextGroup label="Apellido" id="apellido" onChange={handleChange} />
-  </FlexBox>
-)
+const UpdateForm = () => {
+  const user = useSelector(selectUser)
+  const [formData, setFormData] = useState({ ...user })
+
+  return (
+    <FlexBox as="form">
+      <InputTextGroup
+        value={formData.name}
+        label="Nombre"
+        id="nombre"
+        onChange={e => setFormData({ ...formData, name: e.target.value })}
+      />
+      <InputTextGroup
+        value={formData.surnames.first}
+        label="Apellido"
+        id="apellido"
+        onChange={e =>
+          setFormData({
+            ...formData,
+            surnames: { ...formData.surnames, first: e.target.value },
+          })
+        }
+      />
+    </FlexBox>
+  )
+}
 
 function Profile() {
   return (
