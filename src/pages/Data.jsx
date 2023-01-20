8169cc746d8b9f652f09ea6091ef2178275e2fbf
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-01-20 21:51:36 +0100

    dispatch data

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getHouses } from '../store/slices/houseSlice'
 import { Body } from '../components/layout'
 
 function Data() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getHouses())
+  }, [dispatch])
+
   return <Body>Mi perfil</Body>
 }
 
