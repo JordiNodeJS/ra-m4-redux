ec052feefaa7e1ebed5663a022f4f024e5fce097
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-01-21 12:26:39 +0100

    houses list

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,16 +1,30 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getHouses } from '../store/slices/houseSlice'
 import { Body } from '../components/layout'
 
 function Data() {
   const dispatch = useDispatch()
+  const { housesList } = useSelector(state => state.houses)
 
   useEffect(() => {
     dispatch(getHouses())
   }, [dispatch])
 
-  return <Body>Mi perfil</Body>
+  useEffect(() => {
+    console.log('houses', housesList)
+  }, [housesList])
+
+  return (
+    <Body>
+      <h1>Lista de casas</h1>
+      <ul>
+        {housesList.map(house => (
+          <li key={house.id}>{house.title}</li>
+        ))}
+      </ul>
+    </Body>
+  )
 }
 
 export default Data
